77036decd3a47c77b99f1e893d9081ae2e0941d2
Takayuki Kaburagi <t.kaburagi@me.com>
2020-02-07 13:23:18 +0900

    second commit

--- main.tf
+++ main.tf
@@ -10,3 +10,17 @@ provider "chef" {
 	client_name  = "my-first-chef-client"
 	key_material = var.key_material
 }
+
+# Create a Chef Environment
+resource "chef_environment" "production" {
+	name = "production"
+}
+
+# Create a Chef Role
+resource "chef_role" "app_server" {
+	name = "app_server"
+
+	run_list = [
+		"recipe[terraform]",
+	]
+}
