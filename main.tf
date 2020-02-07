2888702af58fbea09203bf7f9fb54931567bf764
Takayuki Kaburagi <t.kaburagi@me.com>
2020-02-07 13:18:16 +0900

    second commit

--- main.tf
+++ main.tf
@@ -2,8 +2,11 @@ terraform {
   required_version = "~> 0.12" 
 }
 
-provider "aws" {
-	access_key = var.access_key
-	secret_key = var.secret_key
-	region = var.region
+# Configure the Chef provider
+provider "chef" {
+	server_url = "https://api.chef.io/organizations/kabu/"
+
+	# You can set up a "Client" within the Chef Server management console.
+	client_name  = "my-first-chef-client"
+	key_material = var.key_material
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,3 @@
-variable "access_key" {}
-variable "secret_key" {}
-variable "region" {
-    default = "ap-northeast-1"
+variable "key_material" {
+
 }
